79fa84abaaab543553d2dcbc0f64aea4bbd3b8e5
peihui12 <128306366+peihui12@users.noreply.github.com>
2023-05-31 00:33:23 +0800

    Add files via upload

---
+++ Text.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Assignment;
+
+import java.util.Scanner;
+
+public class Text {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text = "^hkcpzl$^jhv$^jhv$av$bzl$^aol$^johpu$^zayhalnlt,$(ojpod)$pz$av$johpu$opz$(zwpozlsaahi)$dpao$zayvun$pyvu$johpuz.";
+        int shift = 7;
+        System.out.println("Text: " + text);
+        System.out.println("Shift: " + shift);
+
+        String decryptedText = decrypt(text, shift);
+        System.out.println(decryptedText);
+    }
+
+    public static String decrypt(String text, int shift) {
+        String decrypted = "";
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isAlphabetic(c)) {
+                decrypted += (char) (((int) c - shift - 97 + 26) % 26 + 97);
+            } else if (c == '^') {
+                char a = (char) (((int) text.charAt(++i) - shift - 97 + 26) % 26 + 97);
+                decrypted += Character.toUpperCase(a);
+            } else if (c == '(') {
+                int end_index = text.indexOf(')', i);
+                String reversed = new StringBuilder(text.substring(i + 1, end_index)).reverse().toString();
+                for (int j = 0; j < reversed.length(); j++) {
+                    decrypted += (char) (((int) reversed.charAt(j) - shift - 97 + 26) % 26 + 97);
+                }
+                i = end_index;
+            } else if (c == '$') {
+                decrypted += " ";
+            } else {
+                decrypted += c;
+            }
+        }
+        return decrypted;
+    }
+}
